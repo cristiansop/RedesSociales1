c2a09df9a9d136136012ec96e2d0c9076929c1d2
jhoanseb <jhlozano99@gmail.com>
2020-06-07 15:44:22 -0500

    Se agregan el Popup

---
+++ RedesSociales/Views/MessagePopupView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rg.Plugins.Popup.Pages;
+
+namespace RedesSociales.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MessagePopupView : PopupPage
+    {
+        public MessagePopupView()
+        {
+            InitializeComponent();
+        }
+    }
+}

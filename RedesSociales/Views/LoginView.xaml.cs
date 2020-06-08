5de29f891e0cab5f052465b7e1521e1a8dec064e
jhoanseb <jhlozano99@gmail.com>
2020-06-08 15:06:52 -0500

    Unión con lo de cristian

--- RedesSociales/Views/LoginView.xaml.cs
+++ RedesSociales/Views/LoginView.xaml.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using RedesSociales.ViewModels;
 
 namespace RedesSociales.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginView : ContentPage
     {
+        LoginViewModel context = new LoginViewModel();
         public LoginView()
         {
             InitializeComponent();
+            BindingContext = context;
             btn_google.Clicked += Buttom_IniciarSesion_Clicked;
         }
 
